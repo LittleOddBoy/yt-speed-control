1e965cea09b92a5c77c5c6699bbe052cefb6e55e
Amirhosein Kalari <hosein.kalari.1387@gmail.com>
2025-04-25 12:27:12 +0330

    feat: like the video
    now you can like the video by pressing `e`

--- content.js
+++ content.js
@@ -6,5 +6,12 @@ document.addEventListener("keydown", function (event) {
     video.playbackRate = Math.min(video.playbackRate + 0.5, 16);
   } else if (event.key === "[") {
     video.playbackRate = Math.max(video.playbackRate - 0.5, 0.1);
+  } else if (event.key === "e") {
+    // Find and click the Like button
+    const likeButton = document.querySelector('.yt-spec-button-shape-next--size-m.yt-spec-button-shape-next--segmented-start');
+    if (likeButton) {
+      likeButton.click();
+    }
   }
 });
+
